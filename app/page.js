bcec2b043ede8be3eed39b2b9eed1c560aaea75f
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-08-09 03:21:05 +0000

    Start implementing QR Code canvas

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
+import { QRCode } from 'qrcode'
+import React from 'react'
+
 export default function Home() {
+  var canvas = React.useRef()
+
+QRCode.toCanvas(canvas.current, 'sample text', function (error) {
+  if (error) console.error(error)
+  console.log('success!');
+})
   return (<>
   <h1>Wifi QR Code</h1>
-  <p>GREEK5G</p>
+  <canvas id="canvas" ref={canvas}></canvas>
   </>)
 }
