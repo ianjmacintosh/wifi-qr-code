a37bd48d36bf129d6aa7718bb62e34adf3417a22
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-08-11 03:09:46 +0000

    Make comments easier to find

--- src/App.jsx
+++ src/App.jsx
@@ -16,28 +16,41 @@ function App() {
 
   
   const getWifiURI = ({ type, trdisable, ssid, hidden, id, password, publickey }) => {
+    if (typeof ssid === "undefined") {
+      throw new Error("The object you pass to `getWifiURI()` must include an `ssid` key")
+    }
       /*
       The URI is defined by [7] and formatted by the WIFI-qr ABNF rule:
-      WIFI-qr = “WIFI:” [type “;”] [trdisable “;”] ssid “;” [hidden “;”] [id “;”] [password “;”] [publickey “;”] “;”
-      type = “T:” *(unreserved) ; security type
-      trdisable = “R:” *(HEXDIG) ; Transition Disable value
-      ssid = “S:” *(printable / pct-encoded) ; SSID of the network
-      hidden = “H:true” ; when present, indicates a hidden (stealth) SSID is used
-      id = “I:” *(printable / pct-encoded) ; UTF-8 encoded password identifier, present if the password
-      has an SAE password identifier
-      password = “P:” *(printable / pct-encoded) ; password, present for password-based authentication
-      public-key = “K:” *PKCHAR ; DER of ASN.1 SubjectPublicKeyInfo in compressed form and encoded in
-      “base64” as per [6], present when the network supports SAE-PK, else absent
-      printable = %x20-3a / %x3c-7e ; semi-colon excluded
-      PKCHAR = ALPHA / DIGIT / %x2b / %x2f / %x3d
+      WIFI-qr = “WIFI:” [type “;”] [trdisable “;”] ssid “;” [hidden “;”] [id “;”] [password “;”] [publickey “;”] “;”      
       */
 
-    // WIFI-qr = “WIFI:” [type “;”] [trdisable “;”] ssid “;” [hidden “;”] [id “;”] [password “;”] [publickey “;”] “;”
-    let string = 'WIFI:'
-    string += type ? `T:${type};` : ''
-    string += `S:${ssid}`
-    string += password ? `P:${password};` : ''
-    string += hidden ? `H:true` : '' // Apparently the spec says this only can show the network is hidden, not that it can show the network is NOT hidden
+     // Let's get it
+     let string = 'WIFI:'
+     
+     // type = “T:” *(unreserved) ; security type
+     string += type ? `T:${type};` : ''
+     
+     // trdisable = “R:” *(HEXDIG) ; Transition Disable value
+     string += trdisable ? `R:${trdisable};` : ''
+     
+     // ssid = “S:” *(printable / pct-encoded) ; SSID of the network
+     string += `S:${ssid}`
+     
+     // hidden = “H:true” ; when present, indicates a hidden (stealth) SSID is used
+     string += hidden ? `H:true` : '' // Apparently the spec says this only can show the network is hidden, not that it can show the network is NOT hidden
+     
+     // id = “I:” *(printable / pct-encoded) ; UTF-8 encoded password identifier, present if the password
+     // has an SAE password identifier
+     string += id ? `I:${id};` : ''
+     
+     // password = “P:” *(printable / pct-encoded) ; password, present for password-based authentication
+     string += password ? `P:${password};` : ''
+
+     // public-key = “K:” *PKCHAR ; DER of ASN.1 SubjectPublicKeyInfo in compressed form and encoded in
+     // “base64” as per [6], present when the network supports SAE-PK, else absent
+     // printable = %x20-3a / %x3c-7e ; semi-colon excluded
+     // PKCHAR = ALPHA / DIGIT / %x2b / %x2f / %x3d
+     string += publickey ? `K:${publickey};` : ''
     string += ';'
     return string
   }
