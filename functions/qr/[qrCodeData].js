a9d14f752716b6cf4383ad3e2d2fb7c48507131c
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-08-21 03:18:10 +0000

    Pound head against SSR problem

--- functions/qr/[qrCodeData].js
+++ functions/qr/[qrCodeData].js
@@ -1,9 +1,22 @@
+import { toCanvas } from "qrcode";
 import QRCode from "../../src/QRCode.jsx";
-import { jsx as _jsx } from "react/jsx-runtime";
+import React, { createElement } from "react";
+import ReactDOMServer from "react-dom/server";
+
 export function onRequest(context) {
+  const qrCodeEl = `<canvas id="qr-code" role="img" aria-label="QR Code" aria-description="${context.params.qrCodeData}"></canvas>`;
   return new Response(
-    /*#__PURE__*/ _jsx(QRCode, {
-      data: context.params.qrCodeData,
-    }),
+    `
+<html>
+<body>
+  <div id="root">${qrCodeEl}</div>
+</body>
+</html>
+    `,
+    {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    },
   );
 }
