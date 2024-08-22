1e4a39828b4c487fec29fc31f8a33b0e4abf9d3e
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-08-22 17:23:23 +0000

    Support custom options for QRCode

--- src/QRCode.jsx
+++ src/QRCode.jsx
@@ -1,15 +1,25 @@
 import { toCanvas } from "qrcode"; // 🚨 This is a reference to the qrcode _library_, not this component you're looking at!
 import React from "react";
 
-const QRCode = ({ data }) => {
+const QRCode = ({ data, options: customOptions = {} }) => {
+  const options = {
+    width: 320,
+    errorCorrectionLevel: "L",
+    ...customOptions,
+  };
   const canvasRef = React.useRef();
   React.useEffect(() => {
-    toCanvas(canvasRef.current, data, {
-      width: 320,
-      errorCorrectionLevel: "L",
-    });
+    toCanvas(canvasRef.current, data, options);
   }, [data]);
-  return <canvas id="qr-code" ref={canvasRef} role="img" aria-label="QR Code" aria-description={data}></canvas>;
+  return (
+    <canvas
+      id="qr-code"
+      ref={canvasRef}
+      role="img"
+      aria-label="QR Code"
+      aria-description={data}
+    ></canvas>
+  );
 };
 
 export default QRCode;
