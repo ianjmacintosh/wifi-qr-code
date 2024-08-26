d15c18d8e5a20fa5f9ec593d41cebdd8708e24f2
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-08-26 23:24:34 +0000

    Fix alignment on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -38,22 +38,22 @@ function App() {
 
   return (
     <div className="container mx-auto p-4">
-      <div className="mb-6 space-y-4">
-        <h1 className="text-4xl font-bold">What's the Wifi Password?</h1>
-        <p>Sick of telling people the wifi info?</p>
-        <p>
-          Enter your network details to get a QR code anyone can scan with their
-          phone to get on the wifi
-        </p>
-        <p>
-          Print it out (or take a screenshot) and show it to anyone who wants to
-          connect
-        </p>
-      </div>
       <div className="flex flex-col lg:flex-row gap-4">
         {" "}
         {/* Outer wrapper to center content */}
         <form className="flex flex-col gap-4 grow">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold">What's the Wifi Password?</h1>
+            <p>Sick of telling people the wifi info?</p>
+            <p>
+              Enter your network details to get a QR code anyone can scan with
+              their phone to get on the wifi
+            </p>
+            <p>
+              Print it out (or take a screenshot) and show it to anyone who
+              wants to connect
+            </p>
+          </div>
           <div>
             {/* https://www.hyperui.dev/components/application-ui/login-forms */}
             <label
