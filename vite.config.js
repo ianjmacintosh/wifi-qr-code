3cf9eb78997fb8c127c5c9c410887fee71fa6493
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-08-26 22:30:45 +0000

    Handle Tailwind build with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [react(), tailwindcss],
 });
